1713bca6554ad9dc8ae6e57bb2b59025865069c3
atulbhatt-system32 <atulbhatt98@gmail.com>
2021-07-14 16:23:35 +0530

    Cities dependency removed

--- src/components/AddVendorForm.js
+++ src/components/AddVendorForm.js
@@ -218,7 +218,7 @@ function AddVendorForm() {
           toast.error(result?.error);
         }
       });
-  }, [addressableId, cities]);
+  }, [addressableId]);
 
   // date picker
   const handleDateChange = (e) => {
