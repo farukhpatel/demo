aef50ffa7535d6187644d4944ee3b9c5b9ffa2b9
Farukh Patel <81892013+farukhpatel@users.noreply.github.com>
2021-08-26 14:29:34 +0530

    ok

--- src/components/AddVendorForm.js
+++ src/components/AddVendorForm.js
@@ -227,9 +227,9 @@ function AddVendorForm() {
     });
   };
   function validateEmail(email) {
-    const re =
-      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+//     const re =
+//       /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     return re.test(email);
   }
   const emailValidator = (email) => {
     setEmailValidate(false);
